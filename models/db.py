1020490aba84debaa79f10e6ff66fd2d88c912ea
Chris Georgi <christopher.georgi.pedrero@ciisa.cl>
2020-12-01 15:43:16 -0300

    Se editan tablas segun nuevo modelo. Se remueve auth

--- models/db.py
+++ models/db.py
@@ -1,91 +1,112 @@
-db = DAL('mysql://root@localhost:8080/pypedrive2')
+db = DAL('mysql://root@localhost:3306/pypedrive2')
 
 from gluon.contrib.appconfig import AppConfig
-from gluon.tools import Auth
+#from gluon.tools import Auth
 from gluon.tools import Crud
-auth = Auth(db)
-auth.define_tables()
+#auth = Auth(db)
+#auth.define_tables()
 crud = Crud(db)
 
 #comentadas por si son necesarias luego
-#db.define_table('user',
-#    Field('name'),
-#    Field('email'),
+db.define_table('user',
+    Field('name',type="string", length=255),
+    Field('email',type="string", length=255),
 #    Field('passwd'),
-#    format='%(name)s')
-
-db.define_table('category',
-    Field('name'),
     format='%(name)s')
 
-db.define_table('item',
-    Field('name',),
-    Field('code'),
-    Field('description', type="string", length=256),
-    Field('unit'),
-    Field('tax',type='double'),
-    Field('owner_id','reference auth_user'),
-    Field('category_id','reference category'),
-    Field('enabled', type='boolean'),
-    Field('add_time', 'datetime', default=request.now),
-    Field('update_time', 'datetime', default=request.now),
+db.define_table('category',
+    Field('name',type="string", length=255),
+    Field('status', type='integer', default = 1),
     format='%(name)s')
 
 db.define_table('currency',
-    Field('code'),
-    Field('name'),
-    Field('symbol'),
-    Field('decimal_points',type='integer', default=0),
-    Field('status',type='integer', default=1),
-    Field('add_time', 'datetime', default=request.now),
-    Field('update_time', 'datetime', default=request.now),
+    Field('code',type="string", length=5),
+    Field('name',type="string", length=255),
+    Field('symbol',type="string", length=5),
+    Field('decimal_points', type='integer', default=0),
+    Field('status', type='integer', default = 1),
+    format='%(name)s')
+
+db.define_table('product',
+    Field('name',type="string", length=255),
+    Field('code',type="string", length=255),
+    Field('description', type="string", length=255),
+    Field('unit',type="string", length=255),
+    Field('owner_id','reference user'),
+    Field('category_id','reference category'),
+    Field('status', type='integer', default = 1),
     format='%(name)s')
 
 db.define_table('variation',
-    Field('name'),
-    Field('item_id','reference item'),
-    Field('enabled', type='boolean'),
-    Field('description'),
-    Field('add_time', 'datetime', default=request.now),
-    Field('update_time', 'datetime', default=request.now),
+    Field('name',type="string", length=255),
+    Field('product_id','reference product'),
+    Field('status', type='integer', default = 1),
     format='%(name)s')
 
 db.define_table('price',
-    Field('price',type='double'),
-    Field('reference_id'),
-    Field('is_variation', type='boolean', default=False),
+    Field('ammount',type='double'),
     Field('currency_id','reference currency'),
     Field('cost',type='double'),
-    Field('overhead_cost',type='double'),
-    Field('comment', type="string", length=256),
-    Field('add_time', 'datetime', default=request.now),
-    Field('update_time', 'datetime', default=request.now),
+    Field('status', type='integer', default = 1),
     format='%(name)s')
 
-#db.user.email.requires = IS_NOT_IN_DB(db, 'user.email')
-#db.user.email.requires = IS_NOT_EMPTY()
+db.define_table('product_price',
+    Field('price_id','reference price'),
+    Field('product_id','reference product'),
+    Field('overhead_cost',type='double'))
+
+db.define_table('variation_price',
+    Field('name',type="string", length=255),
+    Field('price_id','reference price'),
+    Field('variation_id','reference variation'),
+    format='%(name)s')
+
+db.define_table('tax',
+    Field('product_id','reference product'),
+    Field('name',type="string", length=255),
+    Field('ammount',type='double'),
+    Field('type', type='integer', default=1),
+    Field('status', type='integer', default = 1),
+    format='%(name)s')
+
+db.define_table('activity',
+    Field('prev_status', type="integer"),
+    Field('new_status', type="integer"),
+    Field('user_id','reference user'),
+    Field('category_id','reference category', requires=IS_EMPTY_OR(IS_IN_DB(db, 'category.id'))),
+    Field('product_id','reference product', requires=IS_EMPTY_OR(IS_IN_DB(db, 'product.id'))),
+    Field('variation_id','reference variation', requires=IS_EMPTY_OR(IS_IN_DB(db, 'variation.id'))),
+    Field('price_id','reference price', requires=IS_EMPTY_OR(IS_IN_DB(db, 'price.id'))),
+    Field('currency_id','reference currency', requires=IS_EMPTY_OR(IS_IN_DB(db, 'currency.id'))),
+    Field('tax_id','reference tax', requires=IS_EMPTY_OR(IS_IN_DB(db, 'currency.id'))),
+    Field('status', type="integer"))
+
+db.user.email.requires = IS_NOT_IN_DB(db, 'user.email')
+db.user.email.requires = IS_NOT_EMPTY()
 #db.user.passwd.requires = IS_NOT_EMPTY()
+
+db.activity.user_id.requires = IS_NOT_EMPTY()
+db.activity.new_status.requires = IS_NOT_EMPTY()
 
 db.category.name.requires = IS_NOT_EMPTY()
 
-db.item.name.requires = IS_NOT_EMPTY()
-db.item.owner_id.requires = IS_NOT_EMPTY()
-db.item.add_time.readable = db.item.add_time.writable = False
-db.item.update_time.readable = db.item.update_time.writable = False
+db.product.name.requires = IS_NOT_EMPTY()
+db.product.owner_id.requires = IS_NOT_EMPTY()
 
-db.currency.code.requires = IS_NOT_IN_DB(db, 'currency.code')
 db.currency.code.requires = IS_NOT_EMPTY()
+db.currency.code.requires = IS_NOT_IN_DB(db, 'currency.code')
 db.currency.name.requires = IS_NOT_EMPTY()
-db.currency.add_time.readable = db.currency.add_time.writable = False
-db.currency.update_time.readable = db.currency.update_time.writable = False
 
 db.variation.name.requires = IS_NOT_EMPTY()
-db.variation.item_id.requires = IS_NOT_EMPTY()
-db.variation.add_time.readable = db.variation.add_time.writable = False
-db.variation.update_time.readable = db.variation.update_time.writable = False
+db.variation.product_id.requires = IS_NOT_EMPTY()
 
-db.price.price.requires = IS_NOT_EMPTY()
-db.price.reference_id.requires = IS_NOT_EMPTY()
+db.price.ammount.requires = IS_NOT_EMPTY()
 db.price.currency_id.requires = IS_NOT_EMPTY()
-db.price.add_time.readable = db.price.add_time.writable = False
-db.price.update_time.readable = db.price.update_time.writable = False
+
+db.product_price.price_id.requires = IS_NOT_EMPTY()
+db.product_price.product_id.requires = IS_NOT_EMPTY()
+
+db.variation_price.price_id.requires = IS_NOT_EMPTY()
+db.variation_price.variation_id.requires = IS_NOT_EMPTY()
+
+db.tax.product_id.requires = IS_NOT_EMPTY()
